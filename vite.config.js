78310bb45a65980a811b125534df08bba8bcb19e
Bartosz Załęski <bartosz.zaleski.pv@gmail.com>
2025-12-04 20:53:13 +0100

    Added the vite-plugin-svgr library, to handling svgs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Movies_Browser-Group_Project"
-})
+  plugins: [react(), svgr()],
+  base: "/Movies_Browser-Group_Project",
+});
