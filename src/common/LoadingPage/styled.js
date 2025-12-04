a28a27f50b685dca87b11f139ab17ab41495cc7a
Kantares77 <marcin.kantarski77@gmail.com>
2025-12-04 19:23:12 +0100

    LoadingPage componnet added

---
+++ src/common/LoadingPage/styled.js
@@ -0,0 +1,26 @@
+import styled, { keyframes } from "styled-components";
+import { ReactComponent as LoadingIcon } from "../../images/loadingIcon.svg";
+
+const rotate = keyframes`
+    to {
+        transform: rotate(360deg);
+    }
+`;
+
+export const Container = styled.div`
+    padding: 16px;
+`;
+
+export const Header = styled.h1`
+    font-size: 36px;
+    line-height: 1.2;
+    margin: 0 0 120px;
+`;
+
+export const IconWrapper = styled.div`
+    text-align: center;
+`;
+
+export const Icon = styled(LoadingIcon)`
+    animation: ${rotate} 2s linear infinite;
+`;
