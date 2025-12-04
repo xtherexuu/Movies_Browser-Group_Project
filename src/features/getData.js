e0e50f395f05519b568e6f16492bc2d2f89e0e8b
BartekKamela <bartek@lubnet.pl>
2025-12-04 19:03:52 +0100

    dodano funkcję pobierania danych z API

--- src/features/getData.js
+++ src/features/getData.js
@@ -1,5 +1,41 @@
-const APIkey = "f4abb2c51d8a72a2554557a4a8c06397"
+import axios from "axios";
+
 const APIbaseURL = "https://api.themoviedb.org/3";
+const APIkey = "f4abb2c51d8a72a2554557a4a8c06397";
+const APIlanguage = "en_US";
 const popularMovies = "/movie/popular";
 const popularPeople = "/person/popular";
 
+const apiService = axios.create({ baseURL: `${APIbaseURL}` });
+
+export const getPopularMovies = async () => {
+  try {
+    const response = await apiService.get(`${popularMovies}`,
+      {
+        params: {
+          api_key: `${APIkey}`,
+          language: `${APIlanguage}`,
+        },
+      });
+    return response.data;
+  }
+  catch (error) {
+    console.error("Error fetching movies", error);
+    return [];
+  }
+};
+
+// fetch
+//
+//export const getPopularMovies = async () => {
+//  try {
+//    const response = await fetch(`${APIbaseURL}/movie/popular?api_key=${APIkey}&language=en-US`);
+//    if (!response.ok) {
+//      throw new Error("An error occurred while searching movies.");
+//    }
+//    const data = await response.json();
+//    return data;
+//  } catch (error) {
+//    console.error(error);
+//  }
+//};
