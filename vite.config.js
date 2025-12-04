11e868173a3ec48a9b3855932170845a3920f46c
Kantares77 <marcin.kantarski77@gmail.com>
2025-12-04 22:39:27 +0100

    vite-plugin added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  plugins: [svgr()],
   base: "/Movies_Browser-Group_Project"
 })
