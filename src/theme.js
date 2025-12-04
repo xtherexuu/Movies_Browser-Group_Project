fd0f01088a7f68d063a3e115b1cc266117b27b38
BartekKamela <bartek@lubnet.pl>
2025-12-04 13:53:49 +0100

    additional colors and font weights added

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,15 @@
 export const theme = {
-    color: {
-      black: "#000000",
-      whisper: "#F5F5FA",
-      white: "#FFFFFF",
-    }
+  color: {
+    white: "#FFFFFF",
+    whisper: "#F5F5FA",
+    mystic: "#E4E6F0",
+    waterloo: "#7E839A",
+    black: "#000000",
+  },
+  fontWeight: {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    semiBold: 600,
+  },
 }
