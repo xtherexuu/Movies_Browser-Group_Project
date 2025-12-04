8c1cb0e1b5d719a293013faeaa7bb353803a5e2b
BartekKamela <bartek@lubnet.pl>
2025-12-04 19:12:18 +0100

    Redux Provider added

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from 'react-redux'
+import store from './store.js'
 import { HashRouter, Route, Routes } from "react-router";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <Provider store={store}>
     <HashRouter>
       <Routes>
         <Route path="/" element={<App />}></Route>
       </Routes>
     </HashRouter>
-  </StrictMode>
+  </Provider>
 );
