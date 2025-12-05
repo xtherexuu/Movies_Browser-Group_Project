655aac11b30811c865f336202738913869ad2570
Kantares77 <marcin.kantarski77@gmail.com>
2025-12-05 21:04:35 +0100

    component NoResultsPage added

---
+++ src/common/NoResultsPage/index.jsx
@@ -0,0 +1,10 @@
+import { Container, Header, IconWrapper, Image } from "./styled";
+
+export const NoResultsPage = () => (
+    <Container>
+        <Header>Sorry, there are no results for</Header>
+        <IconWrapper>
+            <Image />
+        </IconWrapper>
+    </Container>
+);
