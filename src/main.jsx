22b33ef567a554d61d3143a4b52856e4a2153b2a
Bartosz Załęski <contact@bartoszzaleski.com>
2025-12-05 16:45:09 +0100

    Configured application and prepered for Routing and added the PageLayout with Header component in it.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter, Route, Routes } from "react-router";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+import { GlobalStyle } from "./GlobalStyle";
+import { Normalize } from "styled-normalize";
 import App from "./App.jsx";
+import PageLayout from "./common/PageLayout/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-      </Routes>
-    </HashRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Normalize />
+      <HashRouter>
+        <Routes>
+          <Route element={<PageLayout />}>
+            <Route path="/" element={<App />} />
+            <Route path="/people" element={<div>PeopleTest</div>} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </ThemeProvider>
   </StrictMode>
 );
