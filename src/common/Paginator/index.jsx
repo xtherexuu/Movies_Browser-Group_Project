4e5ab0ff818a2fc40816481f777b32655cc5e88d
Bartosz Załęski <contact@bartoszzaleski.com>
2025-12-05 20:02:08 +0100

    created a paginator template (work in progress)

--- src/common/Paginator/index.jsx
+++ src/common/Paginator/index.jsx
@@ -1,8 +1,41 @@
 import { useSearchParams } from "react-router";
+import {
+  PaginatorButton,
+  PaginatorSite,
+  PaginatorText,
+  StyledArrowLeft,
+  StyledArrowRight,
+  StyledPaginator,
+} from "./styled";
 
-export default function Paginator({ pagesAmount }) {
+export const Paginator = ({ pagesAmount }) => {
   let [searchParams, setSearchParams] = useSearchParams();
 
-  searchParams.get("page");
-  return <div>Paginator</div>;
-}
+  let currnetPage = searchParams.get("page");
+  return (
+    <StyledPaginator>
+      <PaginatorButton disabled={currnetPage ? currnetPage === 1 : true}>
+        <StyledArrowLeft />
+        <StyledArrowLeft $secound />
+        {"First"}
+      </PaginatorButton>
+      <PaginatorButton disabled={currnetPage ? currnetPage === 1 : true}>
+        <StyledArrowLeft />
+        {"Previous"}
+      </PaginatorButton>
+      <PaginatorText>
+        Page <PaginatorSite as="strong">{currnetPage || 1}</PaginatorSite> of{" "}
+        <PaginatorSite as="strong">{pagesAmount}</PaginatorSite>
+      </PaginatorText>
+      <PaginatorButton disabled={currnetPage === pagesAmount}>
+        {"Next"}
+        <StyledArrowRight />
+      </PaginatorButton>
+      <PaginatorButton disabled={currnetPage === pagesAmount}>
+        {"Last"}
+        <StyledArrowRight />
+        <StyledArrowRight $secound />
+      </PaginatorButton>
+    </StyledPaginator>
+  );
+};
--- src/common/Paginator/styled.js
+++ src/common/Paginator/styled.js
@@ -1,4 +1,38 @@
+import { Link } from "react-router";
+import styled, { css } from "styled-components";
+import ArrowLeft from "../../images/arrowLeft.svg?react";
+import ArrowRight from "../../images/arrowRight.svg?react";
 
 export const StyledPaginator = styled.div`
-    
-`
+  display: flex;
+  gap: 12px;
+`;
+
+export const PaginatorText = styled.p`
+  margin: 0 12px;
+  font-weight: ${({ theme }) => theme.fontWeight.regular};
+  color: ${({ theme }) => theme.color.waterloo};
+`;
+
+export const PaginatorSite = styled(PaginatorText)`
+  font-weight: ${({ theme }) => theme.fontWeight.semiBold};
+  color: ${({ theme }) => theme.color.black};
+`;
+
+export const PaginatorButton = styled(Link)``;
+
+export const StyledArrowLeft = styled(ArrowLeft)`
+  ${({ $secound }) =>
+    $secound &&
+    css`
+      /* display: none; */
+    `}
+`;
+
+export const StyledArrowRight = styled(ArrowRight)`
+  ${({ $secound }) =>
+    $secound &&
+    css`
+      /* display: none; */
+    `}
+`;
