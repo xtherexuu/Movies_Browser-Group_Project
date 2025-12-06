5f4e1d5b44eaf761f058a1f6bd3c4ff6dc576f4e
BartekKamela <bartek@lubnet.pl>
2025-12-06 14:21:51 +0100

    added popular people component

---
+++ src/features/people/PeopleList/index.jsx
@@ -0,0 +1,12 @@
+import { PersonTile } from "../PersonTile";
+import { Wrapper, Header, PeopleContainer } from "./styled"
+
+export const PeopleListPage = () => {
+  return (
+    <Wrapper>
+      <Header>Popular people</Header>
+      <PeopleContainer />
+      <PersonTile />
+    </Wrapper>
+  )
+};
