9c6da8a0efe5a2a9f9812ff4f40b082c321cbc70
Bartosz Załęski <bartosz.zaleski.pv@gmail.com>
2025-12-08 19:22:31 +0100

    added logic to the searchInput

--- src/features/queries/SearchInput/index.jsx
+++ src/features/queries/SearchInput/index.jsx
@@ -1,17 +1,48 @@
+import { useEffect, useState } from "react";
 import {
   SearchButton,
   StyledInput,
   StyledInputWrapper,
   StyledLoupeIcon,
 } from "./styled";
+import { useDispatch, useSelector } from "react-redux";
+import { selectSearch, setSearchInputValue } from "../queriesSlice";
+import { useLocation, useSearchParams } from "react-router";
+import getNewSearchParams from "../../../getNewSearchParams";
 
 const SearchInput = () => {
+  const dispatch = useDispatch();
+  const search = useSelector(selectSearch);
+  const [inputValue, setInputValue] = useState(search);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(setSearchInputValue(inputValue));
+  }, [inputValue]);
+
+  useEffect(() => {
+    if (search === "" && searchParams.get("s")) {
+      setSearchParams(getNewSearchParams(searchParams, ["s", null]));
+    } else if (search !== "") {
+      setSearchParams(getNewSearchParams(searchParams, ["s", search]));
+    }
+  }, [search]);
+
   return (
     <StyledInputWrapper>
       <SearchButton>
         <StyledLoupeIcon />
       </SearchButton>
-      <StyledInput placeholder="Search for movies..." />
+      <StyledInput
+        value={inputValue}
+        onChange={({ target }) => {
+          setInputValue(target.value);
+        }}
+        placeholder={`Search for ${
+          location.pathname.startsWith("/people") ? "People" : "Movies"
+        }...`}
+      />
     </StyledInputWrapper>
   );
 };
