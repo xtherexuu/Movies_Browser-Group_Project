f5339930aaca05f1bd3f87dd01d7f2bbfac8a42a
BartekKamela <bartek@lubnet.pl>
2025-12-09 18:14:15 +0100

    improved photo path import

--- src/features/people/PersonTile/index.jsx
+++ src/features/people/PersonTile/index.jsx
@@ -1,20 +1,20 @@
 import { TileLink, Tile, Picture, Name } from "./styled";
 import noPersonImage from "../../../images/noPersonImage.svg"
-import { APIimage } from "../../getData";
+import { imagesAPIw200 } from "../../getData";
 
-export const PersonTile = (
+export const PersonTile = ({
   id,
   name,
   profile,
-) => {
+}) => {
   return (
     <TileLink to={`/person/?id=${id}`} key={id}>
       <Tile>
         <Picture 
-          src={profile ? `${APIimage}${profile}` : noPersonImage}
-          alt={name}
+          src={profile ? `${imagesAPIw200}${profile}` : noPersonImage}
+          alt={name ? name : null}
         />
-        <Name>{name}</Name>
+        <Name>{name ? name : null}</Name>
       </Tile>
     </TileLink>
   )
