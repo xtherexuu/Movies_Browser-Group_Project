0f5c21024dd1ccfb8e57d1928bc59fd566f9d679
BartekKamela <bartek@lubnet.pl>
2025-12-09 13:07:14 +0100

    added peopleListSaga to rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,10 @@
 import { all } from 'redux-saga/effects';
 import { movieListSaga } from './features/movies/MovieList/movieListSaga';
+import { peopleListSaga } from './features/people/peopleListSaga';
 
 export default function* rootSaga() {
     yield all([
         movieListSaga(),
+        peopleListSaga(),
     ]);
 };
