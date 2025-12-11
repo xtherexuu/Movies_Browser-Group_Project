1c9f4356784de739b050713ea29fef6fb41563e5
BartekKamela <bartek@lubnet.pl>
2025-12-11 19:25:25 +0100

    added Movie Page component with styles

---
+++ src/features/movies/MoviePage/index.jsx
@@ -0,0 +1,13 @@
+import { Container, Content } from "./styled";
+import { Header } from "./BackgroundPoster";
+
+export const MoviePage = () => {
+  return (
+    <Container>
+      <Header />
+      <Content>
+       
+      </Content>
+    </Container>
+  )
+};
---
+++ src/features/movies/MoviePage/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  flex-grow: 1;
+  width: 100%;
+  margin: auto;
+`;
+
+export const Content = styled.section`
+  max-width: 1368px;
+`;
