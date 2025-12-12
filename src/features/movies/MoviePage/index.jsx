b905433e637127d61f858ed736cf621f4982814b
BartekKamela <bartek@lubnet.pl>
2025-12-12 14:55:10 +0100

    added useLocation to get parameter ID from URL and connected API to component using fetchMovieById action

--- src/features/movies/MoviePage/index.jsx
+++ src/features/movies/MoviePage/index.jsx
@@ -1,22 +1,91 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+
+import {
+  fetchMovieById,
+  selectMovieInfo,
+  selectMovieStatus,
+} from "../movieSlice";
+
 import { Container, Content, Wrapper, Title, List } from "./styled";
 import { Header } from "./BackgroundPoster";
 import { AboutMovieTile } from "./AboutMovieTile";
+import { PersonTile } from "../../people/PersonTile";
+
+export const useQueryParameters = (key) => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  return searchParams.get(key);
+};
 
 export const MoviePage = () => {
-  return (
-    <Container>
-      <Header />
-      <Content>
-        <AboutMovieTile />
-        <Wrapper>
-          <Title>Cast</Title>
-          <List></List>
-        </Wrapper>
-        <Wrapper>
-          <Title>Crew</Title>
-          <List></List>
-        </Wrapper>
-      </Content>
-    </Container>
-  )
+  const dispatch = useDispatch();
+  const id = useQueryParameters("id");
+  const movieData = useSelector(selectMovieInfo);
+  const status = useSelector(selectMovieStatus);
+
+  useEffect(() => {
+    dispatch(fetchMovieById(id));
+  }, []);
+
+  if (status === "success") {
+    const movieDescription = movieData.movieDescription;
+    const movieCast = movieData.movieCrew.cast;
+    const movieCrew = movieData.movieCrew.crew;
+
+    return (
+      <Container>
+        {movieDescription.backdrop_path ? (
+          <Header
+            poster={movieDescription.backdrop_path}
+            rate={movieDescription.vote_average}
+            title={movieDescription.title}
+            voteCount={movieDescription.vote_count}
+          />
+        ) : null}
+        <Content>
+          <AboutMovieTile
+            poster={movieDescription.poster_path}
+            title={movieDescription.title}
+            productionCountry={movieDescription.production_countries}
+            date={movieDescription.release_date}
+            release={movieDescription.release_date}
+            genres={movieDescription.genres}
+            voteCount={movieDescription.vote_count}
+            rate={movieDescription.vote_average}
+            overview={movieDescription.overview}
+          />
+          <Wrapper>
+            <Title>Cast</Title>
+            <List>
+              {movieCast.slice(0, 20).map((actor) => (
+                <PersonTile
+                  id={actor.id}
+                  key={`${actor.id}${actor.character}`}
+                  name={actor.name}
+                  profile_path={actor.profile_path}
+                  role={actor.character}
+                />
+              ))}
+            </List>
+          </Wrapper>
+          <Wrapper>
+            <Title>Crew</Title>
+            <List>
+              {movieCrew.slice(0, 10).map((person) => (
+                <PersonTile
+                  key={`${person.id}${person.job}`}
+                  name={person.name}
+                  profile_path={person.profile_path}
+                  role={person.job}
+                  id={person.id}
+                />
+              ))}
+            </List>
+          </Wrapper>
+        </Content>
+      </Container>
+    )
+  };
 };
