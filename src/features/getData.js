284208ac5d9115453ae10e0c67c34bae61fc4b80
BartekKamela <bartek@lubnet.pl>
2025-12-12 14:38:52 +0100

    added functions to fetch movie and cast by ID

--- src/features/getData.js
+++ src/features/getData.js
@@ -5,6 +5,9 @@ const APIkey = "f4abb2c51d8a72a2554557a4a8c06397";
 const APIlanguage = "en_US";
 const popularMovies = "/movie/popular";
 const popularPeople = "/person/popular";
+const movieById = "/movie/"
+
+export const imagesFullApi = "https://image.tmdb.org/t/p";
 
 const apiService = axios.create({ baseURL: `${APIbaseURL}` });
 
@@ -37,7 +40,41 @@ export const getPopularPeople = async () => {
     return response.data;
   }
   catch (error) {
-    console.error("Error fetching movies", error);
+    console.error("Error fetching people", error);
+    return [];
+  }
+};
+
+export const getMovieByID = async (id) => {
+  try {
+    const response = await apiService.get(`${movieById}${id}`,
+      {
+        params: {
+          api_key: `${APIkey}`,
+          language: `${APIlanguage}`,
+        },
+      });
+    return response.data;
+  }
+  catch (error) {
+    console.error("Error fetching movie", error);
+    return [];
+  }
+};
+
+export const getCrewByID = async (id) => {
+  try {
+    const response = await apiService.get(`${movieById}${id}/credits`,
+      {
+        params: {
+          api_key: `${APIkey}`,
+          language: `${APIlanguage}`,
+        },
+      });
+    return response.data;
+  }
+  catch (error) {
+    console.error("Error fetching movie crew.", error);
     return [];
   }
 };
