be4d781cf63a2d8855a02c6969711ded05f2dcba
BartekKamela <bartek@lubnet.pl>
2025-12-12 14:49:30 +0100

    adding reducers from Movie Page to the store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import movieListReducer from "./features/movies/MovieList/movieListSlice";
+import movieReducer from "./features/movies/movieSlice";
 import rootSaga from "./rootSaga"
 
 const sagaMiddleware = createSagaMiddleware();
@@ -8,6 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
     reducer: {
         movieList: movieListReducer,
+        movie: movieReducer,
     },
      middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(sagaMiddleware),
