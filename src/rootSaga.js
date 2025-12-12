ce3681f0d3ad5c82d490ce491345b79d201ac40c
BartekKamela <bartek@lubnet.pl>
2025-12-12 14:48:30 +0100

    connecting the saga from the Movie Page component to rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,10 @@
 import { all } from 'redux-saga/effects';
 import { movieListSaga } from './features/movies/MovieList/movieListSaga';
+import { movieSaga } from './features/movies/movieSaga';
 
 export default function* rootSaga() {
     yield all([
         movieListSaga(),
+        movieSaga(),
     ]);
 };
