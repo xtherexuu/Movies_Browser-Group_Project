6e9b4e9ab0fe88ef3b6895a12b834d9652f764a0
BartekKamela <bartek@lubnet.pl>
2025-12-13 23:18:08 +0100

    added getting the page number from the URL and passing it to the fetching function

--- src/features/movies/MovieList/index.jsx
+++ src/features/movies/MovieList/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { useLocation } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import {
   fetchMovies,
@@ -11,6 +12,12 @@ import { Paginator } from "../../../common/Paginator";
 import { LoadingPage } from "../../../common/LoadingPage";
 import { ErrorPage } from "../../../common/ErrorPage";
 
+export const useQueryParameters = (key) => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  return searchParams.get(key);
+};
+
 export const MovieListPage = () => {
   const status = useSelector(selectMoviesStatus);
   const data = useSelector(selectMovies);
@@ -19,9 +26,15 @@ export const MovieListPage = () => {
   // API page limit 500
   const APIpagesLimit = 500;
   const pageTitle = "Popular movies";
+  const page = useQueryParameters("page");
+
+  const setPage = (page) => {
+    if (page === null) return 1;
+    else return page;
+  };
 
   useEffect(() => {
-    dispatch(fetchMovies());
+    dispatch(fetchMovies(setPage(page)));
   }, []);
 
   if (status === "loading") {
