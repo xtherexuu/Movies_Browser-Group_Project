5f4602ca277589d14756e9a34aebd7bc56c46144
BartekKamela <bartek@lubnet.pl>
2025-12-13 18:47:59 +0100

    the name and location of files in the directory structure have been changed

--- src/features/movies/moviesListSaga.js
+++ src/features/movies/moviesListSaga.js
@@ -1,6 +1,6 @@
 import { takeLatest, call, put, delay } from "redux-saga/effects";
-import { fetchMovies, fetchMoviesSuccess, fetchMoviesError } from "./movieListSlice";
-import { getPopularMovies } from "../../getData";
+import { fetchMovies, fetchMoviesSuccess, fetchMoviesError } from "./moviesListSlice";
+import { getPopularMovies } from "../getData";
 
 const loadingDelay = 2_000;
 
