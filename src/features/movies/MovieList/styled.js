c3bbdd626c3fb8f019713f43e989a12781f295f8
BartekKamela <bartek@lubnet.pl>
2025-12-13 17:03:20 +0100

    changed html tags in elements

--- src/features/movies/MovieList/styled.js
+++ src/features/movies/MovieList/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.main`
   flex-grow: 1;
   width: 100%;
   max-width: 1368px;
@@ -14,7 +14,7 @@ export const Header = styled.h1`
   margin-bottom: 24px;
 `;
 
-export const MoviesContainer = styled.div`
+export const MoviesContainer = styled.section`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(316px, 1fr));
   gap: 24px;
