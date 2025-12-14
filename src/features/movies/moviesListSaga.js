4eefc462802e675c4d5e5eab09fce6ff3635b406
BartekKamela <bartek@lubnet.pl>
2025-12-14 12:22:27 +0100

    disable loading delay

--- src/features/movies/moviesListSaga.js
+++ src/features/movies/moviesListSaga.js
@@ -2,11 +2,11 @@ import { takeLatest, call, put, delay } from "redux-saga/effects";
 import { fetchMovies, fetchMoviesSuccess, fetchMoviesError } from "./moviesListSlice";
 import { getPopularMovies } from "../getData";
 
-const loadingDelay = 2_000;
+//const loadingDelay = 2_000;
 
 function* fetchMoviesHandler({ payload }) {
     try {
-        yield delay(loadingDelay); //just to demo the loading
+        //yield delay(loadingDelay); //just to demo the loading
         const MoviesDate = yield call(getPopularMovies, payload);
         yield put(fetchMoviesSuccess(MoviesDate));
     } catch (error) {
