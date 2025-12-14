7a015b866809545717e1e36d916a28ed9a67699d
Kantares77 <marcin.kantarski77@gmail.com>
2025-12-14 21:57:13 +0100

    rootSaga updated by adding peopoleDetailsSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,10 @@
 import { all } from 'redux-saga/effects';
 import { movieListSaga } from './features/movies/MovieList/movieListSaga';
+import { peopleDetailsSaga } from './features/people/peopleDetailsSaga';
 
 export default function* rootSaga() {
     yield all([
         movieListSaga(),
+        peopleDetailsSaga(),
     ]);
 };
