d57281ab419d815f2d86eef53d9d4e701c959b62
Kantares77 <marcin.kantarski77@gmail.com>
2025-12-14 21:56:32 +0100

    store updated by adding peopleDetailsReducer

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import movieListReducer from "./features/movies/MovieList/movieListSlice";
+import peopleDetailsReducer from "./features/people/peopleDetailsSlice";
 import rootSaga from "./rootSaga"
 
 const sagaMiddleware = createSagaMiddleware();
@@ -8,6 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
     reducer: {
         movieList: movieListReducer,
+        peopleDetails: peopleDetailsReducer,
     },
      middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(sagaMiddleware),
