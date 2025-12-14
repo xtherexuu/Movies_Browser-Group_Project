45822e1c03faedffbeaae4935b685507429e147b
BartekKamela <bartek@lubnet.pl>
2025-12-14 23:54:52 +0100

    searchSaga added to the rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,10 @@
 import { all } from 'redux-saga/effects';
 import { movieListSaga } from './features/movies/moviesListSaga';
+import { searchSaga } from './features/movies/movieSearchSaga';
 
 export default function* rootSaga() {
     yield all([
         movieListSaga(),
+        searchSaga(),
     ]);
 };
