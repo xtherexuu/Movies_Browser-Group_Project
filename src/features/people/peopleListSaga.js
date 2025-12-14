953fad8d99f7dd9fc0f78e73f6bab5ba67252d15
BartekKamela <bartek@lubnet.pl>
2025-12-14 12:27:28 +0100

    disable loading delay

--- src/features/people/peopleListSaga.js
+++ src/features/people/peopleListSaga.js
@@ -2,11 +2,11 @@ import { takeLatest, call, put, delay } from "redux-saga/effects";
 import { fetchPeople, fetchPeopleSuccess, fetchPeopleError } from "./peopleListSlice";
 import { getPopularPeople } from "../getData";
 
-const loadingDelay = 2_000;
+// const loadingDelay = 2_000;
 
 function* fetchPeopleHandler({ payload }) {
     try {
-        yield delay(loadingDelay); //just to demo the loading
+        // yield delay(loadingDelay); //just to demo the loading
         const PeopleDate = yield call(getPopularPeople, payload);
         yield put(fetchPeopleSuccess(PeopleDate));
     } catch (error) {
