20ff2886a13bcabfb54281e724b58f891bd2d07e
Kantares77 <marcin.kantarski77@gmail.com>
2025-12-14 21:52:56 +0100

    component PeopleDetails added

---
+++ src/features/people/PeopleDetails/index.jsx
@@ -0,0 +1,66 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router";
+import { useEffect } from "react";
+import {
+    selectFetchDataStatus,
+    selectPersonDetails,
+    selectPersonMovieCast,
+    selectPersonMovieCrew,
+    fetchPeopleDetails,
+    selectGenreList,
+} from "../peopleDetailsSlice";
+
+import { PersonPage } from "../PersonPage";
+import { LoadingPage } from "../../../common/LoadingPage";
+import { ErrorPage } from "../../../common/ErrorPage";
+import { MovieList } from "../../movies/MovieList";
+import { StyledMain } from "./styled";
+
+
+const formatDate = (dateString) => {
+    if (!dateString) return "";
+    const [year, month, day] = dateString.split("-");
+    return `${day}.${month}.${year}`;
+};
+
+export const PeopleDetails = () => {
+    const dispatch = useDispatch();
+    const personId = useParams();
+    const fetchDataStatus = useSelector(selectFetchDataStatus);
+    const personDetails = useSelector(selectPersonDetails);
+    const movieCast = useSelector(selectPersonMovieCast);
+    const movieCrew = useSelector(selectPersonMovieCrew);
+    const genereList = useSelector(selectGenreList);
+
+    useEffect(() => {
+        dispatch(fetchPeopleDetails(personId));
+    }, [dispatch, personId]);
+
+    return (
+        <StyledMain>
+            {fetchDataStatus === "loading" && <LoadingPage />}
+            {fetchDataStatus === "error" && <ErrorPage />}
+            {fetchDataStatus === "success" && (
+                <>
+                    <PersonPage
+                        poster={`https://image.tmdb.org/t/p/w200/${personDetails.profile_path}`}
+                        name={personDetails.name}
+                        birthDate={formatDate(personDetails.birthday)}
+                        birthPlace={personDetails.place_of_birth}
+                        biography={personDetails.biography}
+                    />
+                    <MovieList
+                        header={`Movies - cast`}
+                        MovieList={movieCast}
+                        genereList={genereList}
+                    />
+                    <MovieList
+                        header={`Movies - crew`}
+                        MovieList={movieCrew}
+                        genereList={genereList}
+                    />
+                </>
+            )}
+        </StyledMain>
+    )
+};
