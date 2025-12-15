3fe31a0a32b7dcf0de72bb17d63d5a69282c6423
BartekKamela <bartek@lubnet.pl>
2025-12-15 10:47:41 +0100

    searchPerson component conected to the PeopleList component

--- src/features/people/PeopleList/index.jsx
+++ src/features/people/PeopleList/index.jsx
@@ -1,15 +1,21 @@
 import { useEffect } from "react";
+import { useLocation } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import {
   fetchPeople,
   selectPeople,
   selectPeopleStatus,
 } from "../peopleListSlice";
+import {
+  fetchSearchPerson,
+  selectSearchPersonStatus,
+} from "../personSearchSlice";
 import { PersonTile } from "../PersonTile";
 import { Wrapper, Header, PeopleContainer } from "./styled"
 import { Paginator } from "../../../common/Paginator";
 import { LoadingPage } from "../../../common/LoadingPage";
 import { ErrorPage } from "../../../common/ErrorPage";
+import { SearchPerson } from "../PersonSearch";
 import { useQueryParameters } from "../../queryParameters";
 
 
@@ -18,31 +24,41 @@ export const PeopleListPage = () => {
   const data = useSelector(selectPeople);
   const dispatch = useDispatch();
   const page = useQueryParameters("page");
+  const searchQuery = useQueryParameters("search");
   // API page limit
   const APIpagesLimit = "500";
   const pageTitle = "Popular people";
+  const statusSearchPerson = useSelector(selectSearchPersonStatus);
+  const location = useLocation();
 
   const setPage = (page) => {
     if (page === null) {
-      return 1 
+      return 1
     } else {
       return page
     }
   };
 
+  console.log(location);
+
   useEffect(() => {
-    dispatch(fetchPeople(setPage(page)));
-  }, []);
+    if (location.pathname.includes("/people") && location.search.includes("search")) {
+      dispatch(fetchSearchPerson({ query: searchQuery, page: setPage(page) }));
+    }
+    else
+      dispatch(fetchPeople(setPage(page)));
+  }, [searchQuery, page]);
 
-  if (status === "loading") {
-    return (<LoadingPage title={pageTitle} />)
-  }
 
-  if (status === "error") {
-    return (<ErrorPage />)
-  }
+  if (status === "loading") return <LoadingPage title={pageTitle} />;
+  if (statusSearchPerson === "loading") return <LoadingPage title={"Search person"} />
+  if (status === "error") return <ErrorPage />;
+  if (statusSearchPerson === "error") return <Error />;
+  if (searchQuery !== null && statusSearchPerson === "success")
+    return <SearchPerson />;
 
-  if (status === "success") {
+
+  if (status === "success" && searchQuery === null) {
     const pagesAmount = data.total_pages;
     const setPagesAmountMax = () => {
       if (pagesAmount < APIpagesLimit)
