c7080eee77ef00286ebfe65d0790062279d48b1d
Bartosz Załęski <bartosz.zaleski.pv@gmail.com>
2025-12-15 20:45:30 +0100

    changed the react-router logic in a way that app can use react-router loaders

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,46 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store.js";
-import { HashRouter, Route, Routes } from "react-router";
+import {
+  createHashRouter,
+  createRoutesFromElements,
+  redirect,
+  Route,
+  RouterProvider,
+} from "react-router";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
+import { theme } from "./theme.js";
 import { GlobalStyle } from "./GlobalStyle";
 import { Normalize } from "styled-normalize";
 import App from "./App.jsx";
-import PageLayout from "./common/PageLayout/index.jsx";
+import { MovieListPage } from "./features/movies/MovieList/index.jsx";
+
+const loader = ({ request }) => {
+  const { searchParams, pathname } = new URL(request.url);
+  if (searchParams.get("s") || searchParams.get("page")) {
+    throw redirect(pathname);
+  }
+};
+
+const router = createHashRouter(
+  createRoutesFromElements(
+    <Route element={<App />}>
+      <Route path="/" element={<MovieListPage />} />
+      <Route path="/movies" element={<MovieListPage />} />
+      <Route
+        path="/movies/:id"
+        loader={loader}
+        element={<div>test movies specific</div>}
+      />
+      <Route path="/people" element={<div>PeopleTest</div>} />
+      <Route
+        path="/people/:id"
+        loader={loader}
+        element={<div>test people specific</div>}
+      />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -16,14 +49,7 @@ createRoot(document.getElementById("root")).render(
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Normalize />
-        <HashRouter>
-          <Routes>
-            <Route element={<PageLayout />}>
-              <Route path="/" element={<App />} />
-              <Route path="/people" element={<div>PeopleTest</div>} />
-            </Route>
-          </Routes>
-        </HashRouter>
+        <RouterProvider router={router} />
       </ThemeProvider>
     </Provider>
   </StrictMode>
