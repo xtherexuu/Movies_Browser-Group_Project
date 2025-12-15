6327da867d7502d7462a7a5c2d4195ced5247f4e
Bartosz Załęski <bartosz.zaleski.pv@gmail.com>
2025-12-15 20:46:20 +0100

    added new saga for fetching data on popular people/movies sites

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,7 +1,8 @@
 import { all } from "redux-saga/effects";
 import { movieListSaga } from "./features/movies/MovieList/movieListSaga";
 import searchInputSaga from "./features/queries/SearchInput/searchInputSaga";
+import queriesFetchSaga from "./features/queries/queriesFetchSaga";
 
 export default function* rootSaga() {
-  yield all([movieListSaga(), searchInputSaga()]);
+  yield all([movieListSaga(), searchInputSaga(), queriesFetchSaga()]);
 }
