3a5ecc652dede8a7ae510a324f920d58034dff66
BartekKamela <bartek@lubnet.pl>
2025-12-15 10:13:57 +0100

    searchPersonSaga to the rootSaga added

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,10 +1,12 @@
 import { all } from 'redux-saga/effects';
 import { movieListSaga } from './features/movies/MovieList/movieListSaga';
 import { peopleListSaga } from './features/people/peopleListSaga';
+import { searchPersonSaga } from './features/people/personSearchSaga';
 
 export default function* rootSaga() {
     yield all([
         movieListSaga(),
         peopleListSaga(),
+        searchPersonSaga(),
     ]);
 };
