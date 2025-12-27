6b341f02aec34b1ea2d9fa79743c32f56a9371d3
Bartosz Załęski <contact@bartoszzaleski.com>
2025-12-27 19:08:35 +0100

    Added the PersonPage to the routes

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import App from "./App.jsx";
 import PageLayout from "./common/PageLayout/index.jsx";
 import { PeopleListPage } from "./features/people/PeopleList/index.jsx";
 import { MovieListPage } from "./features/movies/MovieList/index.jsx";
+import PersonPage from "./features/people/PersonPage/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -24,6 +25,7 @@ createRoot(document.getElementById("root")).render(
               <Route path="/" element={<MovieListPage />} />
               <Route path="/movies" element={<MovieListPage />} />
               <Route path="/people" element={<PeopleListPage />} />
+              <Route path="/people/:id" element={<PersonPage />} />
             </Route>
           </Routes>
         </HashRouter>
